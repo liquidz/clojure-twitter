b63addc5e7b757526dec9e8fd0d393675c69c0b3
liquidz <masa@uopc.(none)>
2010-05-16 23:57:32 +0900

    update

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject
-  org.clojars.liquidz/twitter "0.0.1"
+  org.clojars.liquidz/twitter "0.0.2"
   :description "twitter4j wrapper for clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.liquidz/simply "0.0.7"]
+                 [org.clojars.liquidz/simply "0.0.8"]
                  [org.twitter4j/twitter4j-core "2.1.2"]
                  ]
             )
--- test/twitter_test.clj
+++ test/twitter_test.clj
@@ -5,15 +5,17 @@
   )
 
 (deftest mytest-test
+  (comment
   (let [sr (twitter-search "#teatime")]
     (println "max id = " (:max-id sr))
     ;(foreach #(println (to-utf8 (:text %)) " (id = " (:id %) ")") (:tweets sr))
     )
+    )
 
-  (let [res (twitter-search-all "#teatime" :lang "ja")]
+  (let [res (twitter-search-all "#yuruyomi")]
     (println "max id = " (:max-id res))
     (println "page = " (:page res))
     (println "tweets count = " (count (:tweets res)))
-    ;(foreach #(println (to-utf8 (:text %))) (take 10 (:tweets res)))
+    (foreach #(println (to-utf8 (:text %))) (take 10 (:tweets res)))
     )
   )
