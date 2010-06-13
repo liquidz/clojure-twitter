82de596b154c7927ddc8790b1ba787875b1d4d73
liquidz <liquidz.uo@gmail.com>
2010-06-13 09:52:50 +0900

    show-twitter-statusを追加

--- test/twitter_test.clj
+++ test/twitter_test.clj
@@ -5,12 +5,12 @@
   )
 
 (deftest mytest-test
-  (let [sr (twitter-search-all "#yuruyomi" :since-id 15562499949)]
-  ;(let [sr (twitter-search "#teatime" :rpp 3)]
-    (println "max id = " (:max-id sr))
-    ;(foreach #(println (to-utf8 (:text %)) " (id = " (:id %) ")") (:tweets sr))
-    (foreach #(println (to-utf8 (:text %)) " (id = " (:id %) ")")
-             (sort #(< (:id %1) (:id %2)) (:tweets sr)))
+;  (let [sr (twitter-search-all "#yuruyomi" :since-id 15562499949)]
+;    (println "max id = " (:max-id sr))
+;    ;(foreach #(println (to-utf8 (:text %)) " (id = " (:id %) ")") (:tweets sr))
+;    (foreach #(println (to-utf8 (:text %)) " (id = " (:id %) ")")
+;             (sort #(< (:id %1) (:id %2)) (:tweets sr)))
+;    )
 
 ;  (let [res (twitter-search-all "#yuruyomi")]
 ;    (println "max id = " (:max-id res))
@@ -19,6 +19,7 @@
 ;    (foreach #(println (to-utf8 (:text %))) (take 10 (:tweets res)))
 ;    )
 
-    (foreach #(println (first %) " => " (second %)) (get-twitter-rate-limit))
-    )
+;  (foreach #(println (first %) " => " (second %)) (get-twitter-rate-limit))
+
+  (p show-twitter-status 15562499950)
   )
